9dd3078a914bae176eca01e37973c636c9c81842
dnyama <127705331+dnyama@users.noreply.github.com>
2024-09-23 11:38:17 +0900

    JavaScript課題（kadai_005）をアップ

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+
+//体重の変数の宣言
+let bodyWeight;
+
+//体重の値の代入
+bodyWeight = 68;
+
+//身長の変数の宣言
+let height;
+
+//身長の値の代入
+height = 1.7;
+
+//BMI値の変数の宣言・値の代入
+let bmi = bodyWeight / (height * height);
+
+// BMI値のコンソール出力
+console.log(bmi);
+
+
